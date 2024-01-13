e282fc1889cca7cdc2ed93ecb37594bdf9c28386
saivishnu90001 <153482167+saivishnu90001@users.noreply.github.com>
2024-01-13 08:06:21 +0530

    Add files via upload

---
+++ Multiplication.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Python program to multiply two number
+
+# take inputs
+num1 = 3
+num2 = 5
+
+# calculate product
+product = num1*num2
+
+# print multiplication value
+print("The Product of Number:", product)
+
+
+# In[ ]:
+
+
+
+
